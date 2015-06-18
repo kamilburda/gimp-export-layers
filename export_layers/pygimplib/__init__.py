3569cf45437fc343d7bf7df5cb455d487b7640aa
khalim19 <khalim19@gmail.com>
2015-06-18 11:07:14 +0200

    Update years in license statements

--- export_layers/pygimplib/__init__.py
+++ export_layers/pygimplib/__init__.py
@@ -2,7 +2,7 @@
 #
 # pygimplib - Collection of modules to improve development of Python GIMP plug-ins
 #
-# Copyright (C) 2014 khalim19 <khalim19@gmail.com>
+# Copyright (C) 2014, 2015 khalim19 <khalim19@gmail.com>
 #
 # pygimplib is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
