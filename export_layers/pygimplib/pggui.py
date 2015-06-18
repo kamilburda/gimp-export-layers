74a485699102d933e1704c38110295b7fd99fd93
khalim19 <khalim19@gmail.com>
2015-06-18 16:55:57 +0200

    Minor cleanup

--- export_layers/pygimplib/pggui.py
+++ export_layers/pygimplib/pggui.py
@@ -326,8 +326,8 @@ def set_gui_excepthook(plugin_title, report_uri_list=None, parent=None):
           exception_message = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
           display_exception_message(exception_message, plugin_title=plugin_title,
                                     report_uri_list=report_uri_list, parent=parent)
-          # Make sure to quit the application since unhandled exceptions
-          # can mess up the application state.
+          # Make sure to quit the application since unhandled exceptions can
+          # mess up the application state.
           if gtk.main_level() > 0:
             gtk.main_quit()
         
