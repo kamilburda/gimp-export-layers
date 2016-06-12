e83edea432f16ed6a2c9edcaa6da70c928d75eb5
khalim19 <khalim19@gmail.com>
2016-06-12 18:49:53 +0200

    pygimplib: Create module containing constants

---
+++ export_layers/pygimplib/constants.py
@@ -0,0 +1,33 @@
+#
+# This file is part of pygimplib.
+#
+# Copyright (C) 2014, 2015 khalim19 <khalim19@gmail.com>
+#
+# pygimplib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pygimplib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pygimplib.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+This module contains constants used in other modules.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+str = unicode
+
+#===============================================================================
+
+_LOG_OUTPUT_MODES = (LOG_EXCEPTIONS_ONLY, LOG_OUTPUT_FILES, LOG_OUTPUT_GIMP_CONSOLE) = (0, 1, 2)
