54a78b07973e34a2d2a4a0ec7b14d608e056f614
khalim19 <khalim19@gmail.com>
2016-08-25 21:06:23 +0200

    log_output: Fix debug logging to files, refactor
    * `stdout` and `stderr` are now also replaced directly rather than "tee"-ed when logging to files, to avoid `IOError` wtih Errno 9. The IOError was triggered by calling `flush()` from the streams (whether the internal buffer was full or not).
    * Merged `tee` into `log_output`.

--- export_layers/pygimplib/log_output.py
+++ export_layers/pygimplib/log_output.py
@@ -18,8 +18,10 @@
 #
 
 """
-This module provides logging of unhandled exceptions and debug information (if
-debugging is enabled).
+This module:
+* provides logging of unhandled exceptions and debug information (if
+debugging is enabled),
+* defines a class to duplicate ("tee") standard output or error output.
 """
 
 from __future__ import absolute_import
@@ -29,6 +31,7 @@ from __future__ import unicode_literals
 
 str = unicode
 
+import datetime
 import logging
 import os
 import sys
@@ -36,36 +39,8 @@ import sys
 from . import constants
 from . import pgpath
 from . import pgpdb
-from . import tee
 
 #===============================================================================
-
-def _logger_add_file_handler(logger, log_paths):
-  """
-  If the first log path in `log_paths` cannot be used (e.g. due to denied write
-  permission), try out subsequent paths.
-  
-  Do not log if directories cannot be created or any of the log files cannot be
-  created.
-  """
-  
-  can_log = True
-  for log_path in log_paths:
-    try:
-      pgpath.make_dirs(os.path.dirname(log_path))
-    except OSError:
-      can_log = False
-      break
-    
-    try:
-      logger.addHandler(logging.FileHandler(log_path))
-    except (OSError, IOError):
-      if log_path == log_paths[-1]:
-        can_log = False
-    else:
-      break
-  
-  return can_log
 
 
 def log_output(log_mode, log_path_dirnames, log_stdout_filename, log_stderr_filename, log_header_title=""):
@@ -108,17 +83,211 @@ def log_output(log_mode, log_path_dirnames, log_stdout_filename, log_stderr_file
       # http://stackoverflow.com/questions/5451746/sys-excepthook-doesnt-work-in-imported-modules/5477639
       # http://bugs.python.org/issue11705
       def log_exceptions(exctype, value, traceback, logger=logger):
-        logger.error(tee.get_log_header(log_header_title).encode(), exc_info=(exctype, value, traceback))
+        logger.error(get_log_header(log_header_title).encode(), exc_info=(exctype, value, traceback))
       
       sys.excepthook = log_exceptions
   
   elif log_mode == constants.LOG_OUTPUT_FILES:
-    tee.Tee(
-      sys.stdout, open(os.path.join(log_path_dirnames[0], log_stdout_filename), "a"),
-      log_header_title=log_header_title, flush_output=True)
-    tee.Tee(
-      sys.stderr, open(os.path.join(log_path_dirnames[0], log_stderr_filename), "a"),
-      log_header_title=log_header_title, flush_output=True)
+    sys.stdout = SimpleLogger(
+      os.path.join(log_path_dirnames[0], log_stdout_filename), "a", log_header_title=log_header_title)
+    sys.stderr = SimpleLogger(
+      os.path.join(log_path_dirnames[0], log_stderr_filename), "a", log_header_title=log_header_title)
   elif log_mode == constants.LOG_OUTPUT_GIMP_CONSOLE:
     sys.stdout = pgpdb.GimpMessageFile(message_delay_milliseconds=20)
     sys.stderr = pgpdb.GimpMessageFile(message_prefix="Error: ", message_delay_milliseconds=20)
+
+
+def _logger_add_file_handler(logger, log_paths):
+  """
+  If the first log path in `log_paths` cannot be used (e.g. due to denied write
+  permission), try out subsequent paths.
+  
+  Do not log if directories cannot be created or any of the log files cannot be
+  created.
+  """
+  
+  can_log = True
+  for log_path in log_paths:
+    try:
+      pgpath.make_dirs(os.path.dirname(log_path))
+    except OSError:
+      can_log = False
+      break
+    
+    try:
+      logger.addHandler(logging.FileHandler(log_path))
+    except (OSError, IOError):
+      if log_path == log_paths[-1]:
+        can_log = False
+    else:
+      break
+  
+  return can_log
+
+
+def get_log_header(log_header_title):
+  return "\n".join(("", "=" * 80, log_header_title, str(datetime.datetime.now()), "\n"))
+
+
+#===============================================================================
+
+
+class SimpleLogger(object):
+  
+  def __init__(self, filename, mode, log_header_title=None):
+    self._log_header_title = log_header_title
+    
+    self._log_file = open(filename, mode)
+  
+  def write(self, data):
+    if self._log_header_title:
+      self._write(get_log_header(self._log_header_title).encode())
+    
+    self._write(data)
+    self.flush()
+    
+    self.write = self._write
+  
+  def _write(self, data):
+    self._log_file.write(data)
+  
+  def flush(self):
+    self._log_file.flush()
+  
+  def close(self):
+    self._log_file.close()
+
+
+#===============================================================================
+
+
+# Original version:
+# http://mail.python.org/pipermail/python-list/2007-May/438106.html
+# Author: Peter Otten
+class Tee(object):
+  
+  """
+  This class copies stdout or stderr output to a specified file,
+  much like the Unix "tee" command.
+  
+  This class acts as a file-like object containing `write` and `flush` methods.
+  
+  Attributes:
+  
+  * `stream` - Either `sys.stdout` or `sys.stderr`. Other objects are invalid
+    and raise `ValueError`.
+    
+  * `log_header_title` - Header text to write when writing into the file
+    for the first time.
+  """
+  
+  def __init__(self, stream, file_object, log_header_title=None, start=True, flush_output=False):
+    """
+    Parameters:
+    
+    * `file_object` - File or file-like object to write to.
+    
+    * `start` - If True, start `Tee` upon instantiation, otherwise don't.
+      To start later, pass `start=False` and call the `start()` method when
+      desired.
+    
+    * `flush_output` - If True, flush output after each write.
+    """
+  
+    self._streams = {sys.stdout: 'stdout', sys.stderr: 'stderr'}
+    
+    self.log_header_title = log_header_title if log_header_title is not None else ""
+    self.flush_output = flush_output
+    
+    self._file = None
+    self._is_running = False
+    
+    self._orig_stream = None
+    self._stream_name = ""
+    self._stream = None
+    
+    self.stream = stream
+    
+    if start:
+      self.start(file_object)
+  
+  def __del__(self):
+    if self.is_running():
+      self.stop()
+  
+  @property
+  def stream(self):
+    return self._stream
+  
+  @stream.setter
+  def stream(self, value):
+    self._stream = value
+    if value in self._streams:
+      self._stream_name = self._streams[value]
+    else:
+      raise ValueError("invalid stream; must be sys.stdout or sys.stderr")
+  
+  def start(self, file_object):
+    """
+    Start `Tee` if not started during the object instantiation.
+    
+    Parameters:
+    
+    * `file_object` - File or file-like object to write to.
+    """
+    
+    self._orig_stream = self.stream
+    setattr(sys, self._stream_name, self)
+    
+    self._file = file_object
+    self._is_running = True
+  
+  def stop(self):
+    """
+    Stop `Tee`, i.e. stop writing to the file.
+    """
+    
+    setattr(sys, self._stream_name, self._orig_stream)
+    self._file.close()
+    
+    self._file = None
+    self._is_running = False
+  
+  def is_running(self):
+    """
+    Return True if `Tee` is running (i.e. writing to file), False otherwise.
+    """
+    
+    return self._is_running
+  
+  def write(self, data):
+    """
+    Write output to the stream and the specified file.
+    
+    If `log_header_title` is not empty, write the log header before the first
+    output.
+    """
+    
+    if self.log_header_title:
+      self._file.write(get_log_header(self.log_header_title).encode())
+    
+    self._write_with_flush(data)
+    
+    if not self.flush_output:
+      self.write = self._write
+    else:
+      self.write = self._write_with_flush
+  
+  def _write(self, data):
+    self._file.write(data)
+    self._stream.write(data)
+
+  def _write_with_flush(self, data):
+    self._file.write(data)
+    self._file.flush()
+    self._stream.write(data)
+    self._stream.flush()
+  
+  def flush(self):
+    self._file.flush()
+    self._stream.flush()
