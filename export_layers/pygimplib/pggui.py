2283e5d06f0ce3f9f8c86369aa01b63ee642e22e
khalim19 <khalim19@gmail.com>
2016-09-12 15:21:59 +0200

    pggui: Overwrite dialog: Add parent directory in dialog text
    Also, cleanup.

--- export_layers/pygimplib/pggui.py
+++ export_layers/pygimplib/pggui.py
@@ -35,6 +35,7 @@ from __future__ import unicode_literals
 str = unicode
 
 import functools
+import os
 import sys
 import traceback
 
@@ -72,6 +73,11 @@ class GtkDialogOverwriteChooser(overwrite.InteractiveOverwriteChooser):
   when a file about to be saved has the same name as an already existing file.
   """
   
+  _DIALOG_BORDER_WIDTH = 8
+  _DIALOG_HBOX_CONTENTS_SPACING = 10
+  _DIALOG_VBOX_SPACING = 5
+  _DIALOG_ACTION_AREA_SPACING = 8
+  
   def __init__(self, values_and_display_names, default_value, default_response, title="",
                parent=None, use_mnemonics=True):
     
@@ -88,17 +94,23 @@ class GtkDialogOverwriteChooser(overwrite.InteractiveOverwriteChooser):
       title="", role=None, parent=self._parent, flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT)
     self._dialog.set_transient_for(self._parent)
     self._dialog.set_title(self._title)
-    self._dialog.set_border_width(8)
+    self._dialog.set_border_width(self._DIALOG_BORDER_WIDTH)
     self._dialog.set_resizable(False)
     
     self._dialog_icon = gtk.Image()
     self._dialog_icon.set_from_stock(gtk.STOCK_DIALOG_QUESTION, gtk.ICON_SIZE_DIALOG)
+    
     self._dialog_text = gtk.Label("")
+    self._dialog_text.set_line_wrap(True)
+    self._dialog_text.set_use_markup(True)
+    
+    self._dialog_text_event_box = gtk.EventBox()
+    self._dialog_text_event_box.add(self._dialog_text)
     
     self._hbox_dialog_contents = gtk.HBox(homogeneous=False)
-    self._hbox_dialog_contents.set_spacing(10)
+    self._hbox_dialog_contents.set_spacing(self._DIALOG_HBOX_CONTENTS_SPACING)
     self._hbox_dialog_contents.pack_start(self._dialog_icon, expand=False, fill=False)
-    self._hbox_dialog_contents.pack_start(self._dialog_text, expand=False, fill=False)
+    self._hbox_dialog_contents.pack_start(self._dialog_text_event_box, expand=False, fill=False)
     
     if self._use_mnemonics:
       apply_to_all_checkbox_label = _("_Apply action to all files")
@@ -107,33 +119,42 @@ class GtkDialogOverwriteChooser(overwrite.InteractiveOverwriteChooser):
     self._apply_to_all_checkbox = gtk.CheckButton(label=apply_to_all_checkbox_label)
     self._apply_to_all_checkbox.set_use_underline(self._use_mnemonics)
     
-    self._hbox_apply_to_all = gtk.HBox(homogeneous=False)
-    self._hbox_apply_to_all.set_spacing(5)
-    self._hbox_apply_to_all.pack_start(self._apply_to_all_checkbox, expand=False, fill=False)
-    
-    self._dialog.vbox.set_spacing(3)
+    self._dialog.vbox.set_spacing(self._DIALOG_VBOX_SPACING)
     self._dialog.vbox.pack_start(self._hbox_dialog_contents, expand=False, fill=False)
-    self._dialog.vbox.pack_start(self._hbox_apply_to_all, expand=False, fill=False)
+    self._dialog.vbox.pack_start(self._apply_to_all_checkbox, expand=False, fill=False)
     
     self._buttons = {}
     for value, display_name in self.values_and_display_names:
       self._buttons[value] = self._dialog.add_button(display_name, value)
     
-    self._dialog.action_area.set_spacing(8)
+    self._dialog.action_area.set_spacing(self._DIALOG_ACTION_AREA_SPACING)
     
     self._apply_to_all_checkbox.connect("toggled", self._on_apply_to_all_changed)
+    
+    self._is_dialog_text_allocated_size = False
+    self._dialog_text_event_box.connect("size-allocate", self._on_dialog_text_event_box_size_allocate)
     
     self._dialog.set_focus(self._buttons[self.default_value])
   
   def _choose(self, filename):
     if filename is not None:
-      text_filename = _("A file named \"{0}\" already exists.\nWhat would you like to do?").format(filename)
+      filename_root, basename = os.path.split(filename)
+      if filename_root:
+        parent_directory = os.path.split(filename_root)[1]
+        text_choose = (
+          _("A file named \"{0}\" already exists in \"{1}\".").format(basename, parent_directory) + " ")
+      else:
+        text_choose = _("A file named \"{0}\" already exists.").format(basename) + "\n"
     else:
-      text_filename = _("A file with the same name already exists.\nWhat would you like to do?")
+      text_choose = _("A file with the same name already exists.") + "\n"
+    
+    text_choose += _("What would you like to do?")
+    
     self._dialog_text.set_markup(
-      "<span font_size=\"large\"><b>" + gobject.markup_escape_text(text_filename) + "</b></span>")
+      "<span font_size=\"large\"><b>" + gobject.markup_escape_text(text_choose) + "</b></span>")
     
     self._dialog.show_all()
+    
     self._overwrite_mode = self._dialog.run()
     
     if self._overwrite_mode not in self._values:
@@ -145,6 +166,15 @@ class GtkDialogOverwriteChooser(overwrite.InteractiveOverwriteChooser):
   
   def _on_apply_to_all_changed(self, widget):
     self._is_apply_to_all = self._apply_to_all_checkbox.get_active()
+  
+  def _on_dialog_text_event_box_size_allocate(self, dialog_text_event_box, allocation):
+    if not self._is_dialog_text_allocated_size:
+      self._is_dialog_text_allocated_size = True
+      
+      # Make sure the label uses as much width as it can in the dialog.
+      dialog_text_allocation = dialog_text_event_box.get_allocation()
+      dialog_vbox_allocation = self._dialog.vbox.get_allocation()
+      self._dialog_text.set_size_request(dialog_vbox_allocation.width - dialog_text_allocation.x, -1)
 
 
 #===============================================================================
