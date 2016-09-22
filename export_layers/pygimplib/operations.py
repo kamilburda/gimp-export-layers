dda1c9901aa49b5e11dbae2ea2f141b0b17dca42
khalim19 <khalim19@gmail.com>
2016-09-22 09:10:47 +0200

    pygimplib: Add module to simplify executing operations

---
+++ export_layers/pygimplib/operations.py
@@ -0,0 +1,156 @@
+#
+# This file is part of pygimplib.
+#
+# Copyright (C) 2014-2016 khalim19 <khalim19@gmail.com>
+#
+# pygimplib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pygimplib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pygimplib.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+This module provides the means to manipulate a list of operations executed
+sequentially.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+str = unicode
+
+import collections
+import inspect
+
+#===============================================================================
+
+
+class OperationsExecutor(object):
+  
+  def __init__(self):
+    # key: operation group name; value: list of (operation, operation args, operation kwargs) tuples
+    self._operations = collections.OrderedDict()
+    # key: operation group name; value: list of (operation, operation args, operation kwargs) tuples
+    self._foreach_operations = collections.OrderedDict()
+  
+  def add_operation(self, operation_group_names, operation, *operation_args, **operation_kwargs):
+    """
+    Add an operation specified by its function `operation`, its arguments
+    `*operation_args` and keyword arguments `**operation_kwargs``, to the
+    operation groups given by `operation_group_names`.
+    
+    The operation groups are created automatically if no operations were added
+    to them before.
+    """
+    
+    for operation_group_name in operation_group_names:
+      self._init_operation_group(operation_group_name)
+      
+      self._operations[operation_group_name].append((operation, operation_args, operation_kwargs))
+  
+  def add_foreach_operation(self, operation_group_names, foreach_operation,
+                            *foreach_operation_args, **foreach_operation_kwargs):
+    """
+    Add an operation to be executed for each operation in groups given by
+    `operation_group_names`. `foreach_operation` is the function to be
+    executed, along with its arguments `*foreach_operation_args` and keyword
+    arguments `**foreach_operation_kwargs`.
+    
+    By default, `foreach_operation` is executed after each operation. To
+    customize this behavior, use the `yield` statement to specify where it is
+    desired to execute the operation. For example:
+    
+      def foo():
+        print("bar")
+        yield
+        print("baz")
+    
+    first prints "bar", then executes the operation and finally prints "baz".
+    
+    If multiple "for-each" operations are added, they are executed in the order
+    they were added by this method.
+    """
+    
+    for operation_group_name in operation_group_names:
+      self._init_operation_group(operation_group_name)
+      
+      if not inspect.isgeneratorfunction(foreach_operation):
+        def execute_foreach_operation_after_operation(*args, **kwargs):
+          yield
+          foreach_operation(*args, **kwargs)
+        
+        foreach_operation_generator_function = execute_foreach_operation_after_operation
+      else:
+        foreach_operation_generator_function = foreach_operation
+      
+      self._foreach_operations[operation_group_name].append(
+        (foreach_operation_generator_function, foreach_operation_args, foreach_operation_kwargs))
+  
+  def execute(self, operation_group_names, *additional_operation_args, **additional_operation_kwargs):
+    """
+    Execute all operations belonging to the groups in the order given by
+    `operation_group_names`.
+    
+    Additional arguments and keyword arguments to all operations in the group
+    are given by `*additional_operation_args` and
+    `**additional_operation_kwargs`, respectively. `**additional_operation_args`
+    are appended at the end of argument list. If some keyword arguments appear
+    in both the keyword arguments to the `**operation_kwargs` argument in the
+    `add_operation` method and `**additional_operation_kwargs`, the values from
+    the latter override the former.
+    
+    If any of the `operation_group_names` do not exist (i.e. do not have any
+    operations), raise `ValueError`.
+    """
+    
+    def _execute_operation(operation, operation_args, operation_kwargs):
+      args = operation_args + additional_operation_args
+      kwargs = dict(operation_kwargs, **additional_operation_kwargs)
+      return operation(*args, **kwargs)
+    
+    def _execute_operation_with_foreach_operations(operation, operation_args, operation_kwargs):
+      operation_generators = [
+        _execute_operation(*params) for params in self._foreach_operations[operation_group_name]]
+      
+      _execute_foreach_operations_once(operation_generators)
+      
+      while operation_generators:
+        result_from_operation = _execute_operation(operation, operation_args, operation_kwargs)
+        _execute_foreach_operations_once(operation_generators, result_from_operation)
+    
+    def _execute_foreach_operations_once(operation_generators, result_from_operation=None):
+      operation_generators_to_remove = []
+      
+      for operation_generator in operation_generators:
+        try:
+          operation_generator.send(result_from_operation)
+        except StopIteration:
+          operation_generators_to_remove.append(operation_generator)
+      
+      for operation_generator_to_remove in operation_generators_to_remove:
+        operation_generators.remove(operation_generator_to_remove)
+    
+    for operation_group_name in operation_group_names:
+      if operation_group_name not in self._operations:
+        raise ValueError("operation group '{0}' does not exist".format(operation_group_name))
+      
+      for operation, operation_args, operation_kwargs in self._operations[operation_group_name]:
+        if self._foreach_operations[operation_group_name]:
+          _execute_operation_with_foreach_operations(operation, operation_args, operation_kwargs)
+        else:
+          _execute_operation(operation, operation_args, operation_kwargs)
+  
+  def _init_operation_group(self, operation_group_name):
+    if operation_group_name not in self._operations:
+      self._operations[operation_group_name] = []
+      self._foreach_operations[operation_group_name] = []
---
+++ export_layers/pygimplib/tests/test_operations.py
@@ -0,0 +1,266 @@
+#
+# This file is part of pygimplib.
+#
+# Copyright (C) 2014-2016 khalim19 <khalim19@gmail.com>
+#
+# pygimplib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pygimplib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pygimplib.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+str = unicode
+
+import unittest
+
+from .. import operations
+
+#===============================================================================
+
+
+def append_test(list_):
+  list_.append("test")
+
+
+def append_to_list(list_, arg):
+  list_.append(arg)
+  return arg
+
+
+def extend_list(list_, *args):
+  list_.extend(args)
+
+
+def update_dict(dict_, **kwargs):
+  dict_.update(kwargs)
+
+
+def append_to_list_before(list_, arg):
+  list_.append(arg)
+  yield
+
+
+def append_to_list_before_and_after(list_, arg):
+  list_.append(arg)
+  yield
+  list_.append(arg)
+
+
+def append_to_list_before_and_after_execute_twice(list_, arg):
+  list_.append(arg)
+  yield
+  yield
+  list_.append(arg)
+
+def append_to_list_before_middle_after_execute_twice(list_, arg):
+  list_.append(arg)
+  yield
+  list_.append(arg)
+  yield
+  list_.append(arg)
+
+
+def append_to_list_again(list_):
+  arg = yield
+  list_.append(arg)
+
+
+#===============================================================================
+
+
+class TestOperationsExecutorAddOperation(unittest.TestCase):
+  
+  def setUp(self):
+    self.operations_executor = operations.OperationsExecutor()
+  
+  def test_execute_no_operation_for_group(self):
+    with self.assertRaises(ValueError):
+      self.operations_executor.execute(["main_processing"])
+  
+  def test_execute_single_group_single_operation(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_test, test_list)
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertEqual(test_list, ["test"])
+  
+  def test_execute_single_group_single_operation_additional_args(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list)
+    self.operations_executor.execute(["main_processing"], 1)
+    
+    self.assertEqual(test_list, [1])
+  
+  def test_execute_single_group_single_operation_additional_multiple_args(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], extend_list, test_list, 1)
+    self.operations_executor.execute(["main_processing"], 2, 3)
+    
+    self.assertListEqual(test_list, [1, 2, 3])
+  
+  def test_execute_single_group_single_operation_invalid_number_of_args(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1, 2)
+    
+    with self.assertRaises(TypeError):
+      self.operations_executor.execute(["main_processing"])
+  
+  def test_execute_single_group_single_operation_additional_args_invalid_number_of_args(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list)
+    
+    with self.assertRaises(TypeError):
+      self.operations_executor.execute(["main_processing"])
+    
+    with self.assertRaises(TypeError):
+      self.operations_executor.execute(["main_processing"], 1, 2)
+  
+  def test_execute_single_group_single_operation_additional_kwargs_override_former_kwargs(self):
+    test_dict = {}
+    self.operations_executor.add_operation(["main_processing"], update_dict, test_dict, one=1, two=2)
+    self.operations_executor.execute(["main_processing"], two="two", three=3)
+    
+    self.assertDictEqual(test_dict, {"one": 1, "two": "two", "three": 3})
+  
+  def test_execute_single_group_multiple_operations(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_test, test_list)
+    self.operations_executor.add_operation(["main_processing"], extend_list, test_list, 1)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, ["test", 1])
+  
+  def test_execute_multiple_groups_multiple_operations(self):
+    test_dict = {}
+    self.operations_executor.add_operation(
+      ["main_processing", "advanced_processing"], update_dict, test_dict, one=1, two=2)
+    self.operations_executor.add_operation(["main_processing"], update_dict, test_dict, two="two", three=3)
+    
+    self.operations_executor.execute(["main_processing"])
+    self.assertDictEqual(test_dict, {"one": 1, "two": "two", "three": 3})
+    
+    self.operations_executor.execute(["advanced_processing"])
+    self.assertDictEqual(test_dict, {"one": 1, "two": 2, "three": 3})
+    
+    test_dict.clear()
+    self.operations_executor.execute(["main_processing", "advanced_processing"])
+    self.assertDictEqual(test_dict, {"one": 1, "two": 2, "three": 3})
+    
+    test_dict.clear()
+    self.operations_executor.execute(["advanced_processing", "main_processing"])
+    self.assertDictEqual(test_dict, {"one": 1, "two": "two", "three": 3})
+
+
+class TestOperationsExecutorAddForEachOperation(unittest.TestCase):
+  
+  def setUp(self):
+    self.operations_executor = operations.OperationsExecutor()
+  
+  def test_execute_single_foreach_operation_default_execution(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(["main_processing"], append_to_list, test_list, 3)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [1, 3, 2, 3])
+  
+  def test_execute_single_foreach_operation_before_operation(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(["main_processing"], append_to_list_before, test_list, 3)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [3, 1, 3, 2])
+  
+  def test_execute_single_foreach_operation_before_and_after_operation(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_and_after, test_list, 3)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [3, 1, 3, 3, 2, 3])
+  
+  def test_execute_single_foreach_operation_before_and_after_operation_multiple_times(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_and_after_execute_twice, test_list, 3)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [3, 1, 1, 3, 3, 2, 2, 3])
+  
+  def test_execute_multiple_foreach_operations(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(["main_processing"], append_to_list_before, test_list, 3)
+    self.operations_executor.add_foreach_operation(["main_processing"], append_to_list, test_list, 4)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [3, 1, 4, 3, 2, 4])
+  
+  def test_execute_multiple_foreach_operations_complex(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_and_after, test_list, 3)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_and_after_execute_twice, test_list, 4)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(
+      test_list,
+      [3, 4, 1, 3, 1, 4,
+       3, 4, 2, 3, 2, 4])
+  
+  def test_execute_multiple_foreach_operations_even_more_complex(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_and_after, test_list, 3)
+    self.operations_executor.add_foreach_operation(
+      ["main_processing"], append_to_list_before_middle_after_execute_twice, test_list, 4)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(
+      test_list,
+      [3, 4, 1, 3, 4, 1, 4,
+       3, 4, 2, 3, 4, 2, 4])
+  
+  def test_execute_foreach_operation_use_return_value_from_operation(self):
+    test_list = []
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 1)
+    self.operations_executor.add_operation(["main_processing"], append_to_list, test_list, 2)
+    self.operations_executor.add_foreach_operation(["main_processing"], append_to_list_again, test_list)
+    
+    self.operations_executor.execute(["main_processing"])
+    
+    self.assertListEqual(test_list, [1, 1, 2, 2])
