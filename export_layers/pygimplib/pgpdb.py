d8f87f503e4c5525d2c7d596725b80d24f7fec5e
khalim19 <khalim19@gmail.com>
2018-04-05 13:10:31 +0200

    pgpdb: Add function to undo GIMP progress bar suppression

--- export_layers/pygimplib/pgpdb.py
+++ export_layers/pygimplib/pgpdb.py
@@ -474,11 +474,36 @@ class GimpMessageFile(object):
 #===============================================================================
 
 
+_dummy_progress_callback = None
+
+
 def suppress_gimp_progress():
   """
-  Prevent the default progress bar in GIMP from updating by installing dummy
-  progress callbacks.
+  Prevent the default progress bar in GIMP from updating by installing a dummy
+  progress callback. Subsequent calls to this function without a matching call
+  to `unsuppress_gimp_progress` have no effect.
   """
   
-  gimp.progress_install(
+  global _dummy_progress_callback
+  
+  if _dummy_progress_callback:
+    return
+  
+  _dummy_progress_callback = gimp.progress_install(
     pgutils.empty_func, pgutils.empty_func, pgutils.empty_func, pgutils.empty_func)
+
+
+def unsuppress_gimp_progress():
+  """
+  Undo suppression of the default progress bar in GIMP caused by calling
+  `suppress_gimp_progress`. Calling this function before calling
+  `suppress_gimp_progress` has no effect.
+  """
+  
+  global _dummy_progress_callback
+  
+  if not _dummy_progress_callback:
+    return
+  
+  gimp.progress_uninstall(_dummy_progress_callback)
+  _dummy_progress_callback = None
