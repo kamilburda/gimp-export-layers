07939395878c8d7a1df319da93a6deeee7d7f2a8
khalim19 <khalim19@gmail.com>
2018-05-04 11:08:47 +0200

    utils.make_package.sh: Add option to list installers to create

--- resources/utils/make_package.sh
+++ resources/utils/make_package.sh
@@ -5,8 +5,13 @@ function error_usage()
   usage='Usage: make_package [options...]
 
 Options:
+  -d, --dest-dir [DIRECTORY] - destination directory of the created package
   -f, --force - make package even if the repository contains local changes
-  -d, --dest-dir - destination directory of the created package
+  -i, --installers [INSTALLERS] - comma-separated list of installers to create
+    - possible values for installers: "windows", "manual", "all"
+    - default: "all" (creates all installers)
+    - example: -i "windows,manual" creates a Windows installer and a ZIP package for manual install
+    - unrecognized installers are silently ignored
   -h, --help - display this help and exit'
   
   if [ -z "$1" ]; then
@@ -18,13 +23,17 @@ Options:
   exit 1
 } 1>&2
 
-force_if_dirty='False'
+
 destination_dirpath='None'
+force_if_dirty='False'
+installers='all'
+
 
 while [[ "${1:0:1}" = "-" && "$1" != "--" ]]; do
   case "$1" in
-  -f | --force ) force_if_dirty='True';;
   -d | --dest-dir ) shift; destination_dirpath='r"'"$1"'"';;
+  -f | --force ) force_if_dirty='True';;
+  -i | --installers ) shift; installers="$1";;
   -h | --help ) error_usage;;
   * ) [[ "$1" ]] && error_usage "unknown argument: $1";;
   esac
@@ -53,7 +62,7 @@ import make_package
 
 os.chdir(resources_dirpath)
 
-make_package.main(destination_dirpath='"$destination_dirpath"', force_if_dirty='"$force_if_dirty"')
+make_package.main(destination_dirpath='"$destination_dirpath"', force_if_dirty='"$force_if_dirty"', installers='"$installers"')
 
 pdb.gimp_quit(0)
 '
