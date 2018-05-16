e6ed1b11c8b6fb5003083b84c5024207a1c29e3d
khalim19 <khalim19@gmail.com>
2018-05-16 09:24:19 +0200

    pygimplib: Update years in license statements

--- export_layers/pygimplib/pgpdb.py
+++ export_layers/pygimplib/pgpdb.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2014-2017 khalim19
+# Copyright (C) 2014-2018 khalim19
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
