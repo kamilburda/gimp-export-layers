7368e42567c60844c6a6103e320adc4d0d181251
khalim19 <khalim19@gmail.com>
2018-05-25 12:14:22 +0200

    GUI: Remove redundant GUI initialization
    ...because it's now handled by pygimplib.

--- export_layers/gui/gui_plugin.py
+++ export_layers/gui/gui_plugin.py
@@ -379,9 +379,6 @@ class ExportLayersGui(object):
     _setup_output_directory_changed(self._settings, self._image)
   
   def _init_gui(self):
-    if gimp.version >= (2, 10):
-      gimpui.gimp_ui_init()
-    
     self._dialog = gimpui.Dialog(
       title=pygimplib.config.PLUGIN_TITLE, role=pygimplib.config.PLUGIN_NAME)
     self._dialog.set_transient()
