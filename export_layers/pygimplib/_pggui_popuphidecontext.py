594274b0d548390bdfb745eba4df1c2a7c6a9352
khalim19 <khalim19@gmail.com>
2019-01-13 20:51:45 +0100

    PopupHideContext: Allow passing custom callback to hide popup

--- export_layers/pygimplib/_pggui_popuphidecontext.py
+++ export_layers/pygimplib/_pggui_popuphidecontext.py
@@ -41,9 +41,21 @@ class PopupHideContext(object):
   focuses out of the widget that spawned the popup, the popup is hidden.
   """
   
-  def __init__(self, popup_to_hide, popup_owner_widget):
+  def __init__(self, popup_to_hide, popup_owner_widget, hide_callback=None):
+    """
+    Parameters:
+    
+    * `popup_to_hide` - A `gtk.Window` instance representing a popup to hide.
+    
+    * `popup_owner_widget` - A `gtk.Widget` instance that spawned the popup.
+    
+    * `hide_callback` - A function to hide the popup. If `None`, the
+      `popup_to_hide.hide()` method is used to hide the popup.
+    """
     self._popup_to_hide = popup_to_hide
     self._popup_owner_widget = popup_owner_widget
+    self._hide_callback = (
+      hide_callback if hide_callback is not None else self._popup_to_hide.hide)
     
     self._button_press_emission_hook_id = None
     self._toplevel_configure_event_id = None
@@ -60,6 +72,7 @@ class PopupHideContext(object):
     
     toplevel = _pggui_utils.get_toplevel_window(self._popup_owner_widget)
     if toplevel is not None:
+      toplevel.get_group().add_window(self._popup_to_hide)
       # Button presses on the window decoration cannot be intercepted via the
       # `"button-press-event"` emission hooks, hence this workaround.
       self._toplevel_configure_event_id = toplevel.connect(
@@ -84,17 +97,17 @@ class PopupHideContext(object):
     widget.connect("leave-notify-event", self._on_widget_leave_notify_event)
   
   def _on_popup_owner_widget_focus_out_event(self, widget, event):
-    self._popup_to_hide.hide()
+    self._hide_callback()
   
   def _on_emission_hook_button_press_event(self, widget, event):
     if self._widgets_with_entered_pointers:
       return True
     else:
-      self._popup_to_hide.hide()
+      self._hide_callback()
       return False
   
   def _on_toplevel_configure_event(self, toplevel, event):
-    self._popup_to_hide.hide()
+    self._hide_callback()
   
   def _on_widget_enter_notify_event(self, widget, event):
     self._widgets_with_entered_pointers.add(widget)
