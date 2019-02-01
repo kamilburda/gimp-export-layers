5cac7d80e3ec0f18d5e7361eb7302ad9ea05fd12
khalim19 <khalim19@gmail.com>
2019-02-01 18:00:58 +0100

    test_pgitemtree: Move util functions to a separate module

--- export_layers/pygimplib/tests/test_pgitemtree.py
+++ export_layers/pygimplib/tests/test_pgitemtree.py
@@ -37,6 +37,7 @@ except ImportError:
 import mock
 
 from . import stubs_gimp
+from . import utils_pgitemtree
 from .. import pgitemtree
 from .. import pgconstants
 
@@ -58,46 +59,6 @@ class LayerFilterRules(object):
   @staticmethod
   def has_matching_file_extension(layer_elem, file_extension):
     return layer_elem.name.endswith("." + file_extension)
-
-
-def _parse_layers(layer_tree_string):
-  """
-  From a given string containing layer names separated by lines and
-  curly braces (each on a separate line), return an image containing parsed
-  layers.
-  
-  Leading or trailing spaces in each line in the string are truncated.
-  """
-  image = stubs_gimp.ImageStub()
-  
-  layer_tree_string = layer_tree_string.strip()
-  lines = layer_tree_string.splitlines(False)
-  
-  num_lines = len(lines)
-  parents = [image]
-  current_parent = image
-  
-  for i in range(num_lines):
-    current_symbol = lines[i].strip()
-    
-    layer = None
-    
-    if current_symbol.endswith(" {"):
-      layer = stubs_gimp.LayerGroupStub(current_symbol.rstrip(" {"))
-      current_parent.layers.append(layer)
-      current_parent = layer
-      parents.append(current_parent)
-    elif current_symbol == "}":
-      parents.pop()
-      current_parent = parents[-1]
-    else:
-      layer = stubs_gimp.LayerStub(current_symbol)
-      current_parent.layers.append(layer)
-    
-    if layer is not None:
-      layer.parent = current_parent
-  
-  return image
 
 
 @mock.patch(
@@ -145,7 +106,7 @@ class TestLayerTree(unittest.TestCase):
       }
     """
     
-    image = _parse_layers(layers_string)
+    image = utils_pgitemtree.parse_layers(layers_string)
     self.layer_tree = pgitemtree.LayerTree(image)
   
   def test_get_item_tree_element_attributes(self):
